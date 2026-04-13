f068c7ec480c8b043c9373376634fec874485c1d
Rishit Vishwakarma <rishitvishwakarma87@gmail.com>
2026-04-13 18:09:04 +0530

    Update API base URL to production Render backend

--- Info-MedicationTracker-main/src/api.js
+++ Info-MedicationTracker-main/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:8080",
+    baseURL: "https://medication-tracker-gu1i.onrender.com",
     headers: {
         "Content-Type": "application/json",
     },
